420b55e121b5c6405678fcec933c2851bf8e4b51
hanfei.liu <hanfei.liu@xiao-ju.com>
2025-06-12 10:14:18 +0800

    init

--- main_test.go
+++ main_test.go
@@ -26,7 +26,7 @@ func TestRunsSuite(t *testing.T) {
 	//)
 	fixture := acmetest.NewFixture(&volcengineDNSProviderSolver{},
 		acmetest.SetResolvedZone(zone),
-		acmetest.SetManifestPath("testdata/volcengine-dns-solver"),
+		acmetest.SetManifestPath("testdata/volcengine-solver"),
 		acmetest.SetDNSServer("127.0.0.1:59351"),
 		acmetest.SetUseAuthoritative(false),
 	)
